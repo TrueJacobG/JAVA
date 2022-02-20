bb7cf0f308b15d5263504c4913c038a60f7bd024
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-02-20 18:39:53 +0100

    starting with mongoDB

---
+++ mongoDB/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+
+public class Main {
+    public static MongoClient mongoClient;
+    public static DB database;
+    public static DBCollection collection;
+
+
+    public static void main(String[] args) {
+
+    }
+}
